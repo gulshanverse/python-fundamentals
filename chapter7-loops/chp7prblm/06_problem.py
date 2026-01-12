9cdb52e59c1801ed6f1dac818f4b0e3646251b9d
Gulshan Kumar <gulshankumar41309@gmail.com>
2026-01-12 18:50:38 +0530

    Chapter 7 problems solutions.

---
+++ chapter7-loops/chp7prblm/06_problem.py
@@ -0,0 +1,75 @@
+# n = int(input("Enter a number: "))
+# fact = 1
+# for i in range(1,n+1):
+#     fact = fact*i
+# print(f"Factorial of {n} is {fact}")
+
+import math
+print("Factorial: ", math.factorial(5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ chapter7-loops/chp7prblm/07_problem.py
@@ -0,0 +1,30 @@
+'''
+for n = 3
+    * 
+  * * *
+* * * * *
+'''
+n = int(input("Enter the number: "))
+for i in range (1,n+1):
+    print(" "*(n-i),end ="")
+    print("*"*(2*i-1),end ="")
+    print(" ")
+
+'''
+for n = 4
+      * 
+    * * *
+  * * * * *
+* * * * * * *
+'''
+
+
+
+
+
+
+# for i in range(1,3):
+#     for j in range(
+
+#     )
+    
